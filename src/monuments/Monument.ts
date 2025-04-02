ac810f0d2dc367f8e3e25b53880625c7f347b636
Raimon <raimon.txollo@gmail.com>
2025-04-02 02:05:55 +0200

    Add Monuments class, Monuments intances and monuments array

---
+++ src/monuments/Monument.ts
@@ -0,0 +1,21 @@
+import { v4 as uuidv4 } from "uuid";
+import { Location, MonumentStructure } from "./types";
+
+class Monument implements MonumentStructure {
+  public id: string;
+  public city: string;
+  public country: string;
+
+  constructor(
+    public name: string,
+    public description: string,
+    public imageUrl: string,
+    location: Location,
+  ) {
+    this.id = uuidv4();
+    this.city = location.city;
+    this.country = location.country;
+  }
+}
+
+export default Monument;
---
+++ src/monuments/types.ts
@@ -0,0 +1,11 @@
+export interface MonumentStructure {
+  id: string;
+  name: string;
+  description: string;
+  imageUrl: string;
+}
+
+export interface Location {
+  country: string;
+  city: string;
+}
