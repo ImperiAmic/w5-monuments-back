84955841d4d41da6acaaf1339a405b74c7a12918
Raimon <raimon.txollo@gmail.com>
2025-04-06 22:57:22 +0200

    Add addMonument method from MonumentController class

--- src/monument/controller/MonumentController.ts
+++ src/monument/controller/MonumentController.ts
@@ -1,12 +1,41 @@
 import { Request, Response } from "express";
 import { MonumentStructure } from "../types.js";
 import MonumentControllerStructure from "./types.js";
-
+import Monument from "../Monument.js";
 class MonumentController implements MonumentControllerStructure {
   constructor(private monuments: MonumentStructure[]) {}
 
   getMonuments = (_req: Request, res: Response): void => {
     res.status(200).json({ monuments: this.monuments });
+  };
+
+  addMonuments = (req: Request, res: Response): void => {
+    const monument = req.body as MonumentStructure;
+
+    const newMonument = new Monument(
+      monument.name,
+      monument.description,
+      monument.imageUrl,
+      {
+        city: monument.city,
+        country: monument.country,
+      },
+    );
+
+    const isNewMonumentAdded = this.monuments.some(
+      (monument) => monument.name === newMonument.name,
+    );
+
+    if (isNewMonumentAdded) {
+      res
+        .status(409)
+        .json({ error: "A monument with same name is already created" });
+      return;
+    }
+
+    this.monuments.push(newMonument);
+
+    res.status(201).json(newMonument);
   };
 }
 
