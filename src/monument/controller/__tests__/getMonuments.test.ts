79c852022005947b3873ba4f6a72ef468d08a4ad
Raimon <raimon.txollo@gmail.com>
2025-04-06 22:50:51 +0200

    Add test for getMonuments method from MonumentController class

---
+++ src/monument/controller/__tests__/getMonuments.test.ts
@@ -0,0 +1,39 @@
+import { Request, Response } from "express";
+import MonumentController from "../MonumentController.js";
+import { barAmadeo, ermitaCid, iglesuelaMonuments } from "../../fixture.js";
+
+describe("Given the getMonuments method of the class MonumentController", () => {
+  describe("When it receives a request and a response", () => {
+    const req = {} as Request;
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    } as Pick<Response, "status" | "json">;
+
+    beforeEach(() => {
+      jest.clearAllMocks();
+    });
+
+    test("Then it should it call the received response's method status with 200", () => {
+      const expectedStatusCode = 200;
+
+      const monumentController = new MonumentController(iglesuelaMonuments);
+
+      monumentController.getMonuments(req, res as Response);
+
+      expect(res.status).toHaveBeenCalledWith(expectedStatusCode);
+    });
+
+    test("Then it should it call the received response's method json with Ermita del Cid and Bar Amadeo monuments", () => {
+      const expectedMessage = {
+        monuments: [ermitaCid, barAmadeo],
+      };
+
+      const monumentController = new MonumentController(iglesuelaMonuments);
+
+      monumentController.getMonuments(req, res as Response);
+
+      expect(res.json).toHaveBeenCalledWith(expectedMessage);
+    });
+  });
+});
