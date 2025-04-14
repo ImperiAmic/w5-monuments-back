ff3faef50c1ed2b729e22f60a1178941b0460b37
Raimon <raimon.txollo@gmail.com>
2025-04-14 19:22:42 +0200

    Add deleteMonument method to MonumentController class and its endpoint

--- src/monument/controller/MonumentController.ts
+++ src/monument/controller/MonumentController.ts
@@ -37,6 +37,29 @@ class MonumentController implements MonumentControllerStructure {
 
     res.status(201).json(newMonument);
   };
+
+  deleteMonument = (req: Request, res: Response): void => {
+    const monumentId = req.params.monumentId;
+
+    const isMonumentFound = this.monuments.find(
+      (monument) => monument.id === monumentId,
+    );
+
+    if (!isMonumentFound) {
+      res.status(404).json({ error: "Monument to delete has not been found" });
+      return;
+    }
+
+    const monumentPosition = this.monuments.findIndex(
+      (monument) => monument.id === monumentId,
+    );
+
+    const deletedMonument = this.monuments[monumentPosition];
+
+    this.monuments.splice(monumentPosition, 1);
+
+    res.status(200).json(deletedMonument);
+  };
 }
 
 export default MonumentController;
